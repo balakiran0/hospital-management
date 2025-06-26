04bd8541602074aad2ca4766e2e2aab19e8e1104
balakiran0 <balakiran0405@gmail.com>
2025-06-26 11:52:01 +0530

    Add book_appointment and appointment_list views to appointments/views.py

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,20 @@
-from rest_framework import viewsets
+from django.shortcuts import render, redirect
 from .models import Appointment
-from .serializers import AppointmentSerializer
+from .forms import AppointmentForm
+from django.contrib.auth.decorators import login_required
 
-class AppointmentViewSet(viewsets.ModelViewSet):
-    queryset = Appointment.objects.all()
-    serializer_class = AppointmentSerializer
+@login_required
+def book_appointment(request):
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('appointment_list')
+    else:
+        form = AppointmentForm()
+    return render(request, 'appointments/book_appointment.html', {'form': form})
+
+@login_required
+def appointment_list(request):
+    appointments = Appointment.objects.filter(patient__user=request.user)
+    return render(request, 'appointments/appointment_list.html', {'appointments': appointments})
