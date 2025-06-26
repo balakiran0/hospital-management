d699e1d81d5a57e5724ef9fe7ae98e5f8400ce13
balakiran0 <balakiran0405@gmail.com>
2025-06-26 11:53:57 +0530

    Add AppointmentForm to appointments/forms.py

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,1 +1,8 @@
 # forms.py for appointments app
+from django import forms
+from .models import Appointment
+
+class AppointmentForm(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = ['doctor', 'date', 'reason']
