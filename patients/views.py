d4cb7b1ff2a25aa8a83632e59616fcb24e199831
balakiran0 <balakiran0405@gmail.com>
2025-06-26 11:34:12 +0530

    Add PatientViewSet and MedicalHistoryViewSet to patients/views.py

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Patient, MedicalHistory
+from .serializers import PatientSerializer, MedicalHistorySerializer
 
-# Create your views here.
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+class MedicalHistoryViewSet(viewsets.ModelViewSet):
+    queryset = MedicalHistory.objects.all()
+    serializer_class = MedicalHistorySerializer
