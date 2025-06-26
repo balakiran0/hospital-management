453c9d5e07aa98b50009000fdd266185130873db
balakiran0 <balakiran0405@gmail.com>
2025-06-26 11:22:48 +0530

    Fix and update admin registrations in patients/admin.py

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Patient, MedicalHistory
 
-# Register your models here.
+admin.site.register(Patient)
+admin.site.register(MedicalHistory)
+from django.contrib import admin
+from .models import Appointment
+
+admin.site.register(Appointment)
